4a9faeea0d56a76cf58046e313db14be051fe37d
P4duk3s <zygisbalcius@gmail.com>
2025-01-18 20:27:21 +0000

    remove dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from 'react'
+import { useRef } from 'react'
 import './App.css'
 import { Header } from './components/Header'
 import { Main } from './components/Main'
